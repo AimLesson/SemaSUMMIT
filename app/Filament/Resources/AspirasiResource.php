805a262e9b57607eaad18f8deec072602646fa52
Gerson Manuel Sugianto <manuelsugianto@gmail.com>
2025-05-15 07:45:27 +0700

    Add delete action to Aspirasi table actions

--- app/Filament/Resources/AspirasiResource.php
+++ app/Filament/Resources/AspirasiResource.php
@@ -88,6 +88,8 @@ class AspirasiResource extends Resource
             ->actions([
                 Tables\Actions\ViewAction::make(),
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+
             ])
             ->bulkActions([
                 // Tables\Actions\BulkActionGroup::make([
